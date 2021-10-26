f2e1acdbd50cea0db7bad44fdd57c19d7f025097
CaglarMacha <caglar.ayhan@boun.edu.tr>
2021-10-26 16:13:33 +0300

    Product_TrService was refactoring

--- Business/Concrete/Product_TrService.cs
+++ Business/Concrete/Product_TrService.cs
@@ -1,5 +1,7 @@
 ﻿using Business.Abstract;
 using Core.Entities.Concrete;
+using Data.Context.EFCore;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +12,28 @@ namespace Business.Concrete
 {
     public class Product_TrService : IProductService<Product_Tr>
     {
+        private readonly MultitenantDbContext _context;
+
+        public Product_TrService(MultitenantDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Product_Tr> CreateAsync(string dc_Zaman, string dc_Kategori, string dc_Olay)
+        {
+            var product = new Product_Tr(dc_Zaman, dc_Kategori, dc_Olay);
+            _context.Product_Trs.Add(product);
+            await _context.SaveChangesAsync();
+            return product;
+        }
+        public async Task<IReadOnlyList<Product_Tr>> GetAllAsync()
+        {
+            return await _context.Product_Trs.ToListAsync();
+        }
+        public async Task<Product_Tr> GetByIdAsync(int ID)
+        {
+            return await _context.Product_Trs.FindAsync(ID);
+        }
 
 
 
