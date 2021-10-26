8972d7bb375890f4c99cb0bf67fc11631b0b0f8b
CaglarMacha <caglar.ayhan@boun.edu.tr>
2021-10-26 15:53:56 +0300

    Business layer was added

---
+++ Business/Abstract/IProductService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface IProductService<T> where T:class
+    {
+        Task<T> CreateAsync(string name, string description, int rate);
+        Task<T> GetByIdAsync(int id);
+        Task<IReadOnlyList<T>> GetAllAsync();
+    }
+}
