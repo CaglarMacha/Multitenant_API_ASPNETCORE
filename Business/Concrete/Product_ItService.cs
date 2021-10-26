30dc75aba56a75e3a8d84e8eb645de3089215286
CaglarMacha <caglar.ayhan@boun.edu.tr>
2021-10-26 16:07:20 +0300

    product services for all of them was added

---
+++ Business/Concrete/Product_ItService.cs
@@ -0,0 +1,28 @@
+﻿using Business.Abstract;
+using Core.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class Product_ItService : IProductService<Product_It>
+    {
+        public Task<Product_It> CreateAsync(string name, string description, int rate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<Product_It>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product_It> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Business/Concrete/Product_TrService.cs
@@ -0,0 +1,31 @@
+﻿using Business.Abstract;
+using Core.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class Product_TrService : IProductService<Product_Tr>
+    {
+
+
+
+        public Task<Product_Tr> CreateAsync(string name, string description, int rate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<Product_Tr>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product_Tr> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
