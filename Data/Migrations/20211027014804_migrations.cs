9e58cd1f06421d49659635dd8079e1526a804184
CaglarMacha <caglar.ayhan@boun.edu.tr>
2021-10-27 04:48:33 +0300

    migrations was added

---
+++ Data/Migrations/20211027014804_migrations.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.Migrations
+{
+    public partial class migrations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    dc_Zaman = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    dc_Kategori = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    dc_Olay = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TenantId = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
